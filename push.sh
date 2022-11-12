afff2ecf8d0219e1d9ca0c7fdb108ce8750b6c71
wa008 <286436211@qq.com>
2022-11-12 16:07:13 +0800

    kong

--- push.sh
+++ push.sh
@@ -10,5 +10,16 @@ git pull
 
 git add *
 git status 
+
+echo "check commit... Yes or No"
+echo "Yes : 1"
+echo "No  : 0"
+if [ $flag != 1 ] ; then
+	echo "No, exit"
+	exit 1
+else
+    echo "Yes, continue"
+fi
+
 git commit -m "$comm"
 git push origin main
