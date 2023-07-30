30063246b58565fbbd4c89f551c20d3823b829f9
wa008 <286436211@qq.com>
2023-07-30 18:35:21 +0800

    HN

--- _topHackNews/runHackNews.sh
+++ _topHackNews/runHackNews.sh
@@ -8,7 +8,7 @@ ls
 
 exist=0
 index=0
-for file in ./data/*
+for file in ./_topHackNews/data/*
 do
     if [[ ${index} -ge 3 ]]
     then 
@@ -27,7 +27,7 @@ if [[ ${exist} -eq 0 ]]
 then 
     story_file_name=${time}-${story}
     echo ${story_file_name}
-    curl https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty > ./data/${story_file_name}
+    curl https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty > ./_topHackNews/data/${story_file_name}
     echo "${story} write done"
 else 
     echo "${story} already exist"
