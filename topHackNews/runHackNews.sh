7eb075d1f4a7a64b23234ea91b6720c5c800678a
wa008 <286436211@qq.com>
2023-07-31 20:59:47 +0800

    HN update

--- topHackNews/runHackNews.sh
+++ topHackNews/runHackNews.sh
@@ -8,26 +8,29 @@ ls
 
 exist=0
 index=0
-for file in ./_topHackNews/data/* 
-do 
-    if [[ ${index} -ge 3 ]]
+for file in `ls -r ./topHackNews/_posts/`
+do
+    if [[ ${index} -ge 10 ]]
     then 
         break 
     fi 
     history_story=`echo ${file} | awk -F '-' '{print $3}'`
-    if [[ "${history_story}" == "${story}" ]] 
+    echo "history_story: ${history_story}"
+    echo "story: ${story}"
+    if [[ "${history_story}" == "${story}" ]]
     then
         exist=1
         break 
     fi
     let index++
 done
+exit 1
 
 if [[ ${exist} -eq 0 ]]
 then 
     story_file_name=${time}-${story}
     echo ${story_file_name}
-    curl https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty > ./_topHackNews/data/${story_file_name}
+    curl https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty > ./topHackNews/_posts/${story_file_name}
     echo "${story} write done"
     git status
     git add ./_topHackNews/data/${story_file_name}
@@ -36,3 +39,4 @@ else
     echo "${story} already exist"
     date > ./_topHackNews/data/timestamp
 fi
+
