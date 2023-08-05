99dad714d065ae2c4fe2eace987693cdf67f1de0
wa008 <286436211@qq.com>
2023-08-05 20:56:19 +0800

    rename

---
+++ topHackNews/tophacknews.sh
@@ -0,0 +1,37 @@
+# /usr/bin/bash
+
+time=`date '+%Y-%m-%d-%H%M%S'`
+
+url="https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+story=`curl ${url} | awk -F ",| " '{print $2}'`
+ls
+
+exist=0
+index=0
+for file in `ls -r ./tophacknews/_posts/`
+do
+    if [[ ${index} -ge 10 ]]
+    then 
+        break 
+    fi 
+    history_story=`echo ${file} | awk -F '-' '{print $5}' | awk -F '.' '{print $1}'`
+    echo "history_story: ${history_story}"
+    echo "story: ${story}"
+    if [[ "${history_story}" == "${story}" ]]
+    then
+        exist=1
+        break 
+    fi
+    let index++
+done
+
+if [[ ${exist} -eq 0 ]]
+then 
+    story_file_name="${time}-${story}.md"
+    echo ${story_file_name}
+    curl https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty > ./tophacknews/_posts/${story_file_name}
+    echo "${story} write done"
+else 
+    echo "${story} already exist"
+    date > ./tophacknews/_posts/2020-timestamp
+fi
