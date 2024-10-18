9dda6869f9540ab4167c764f44ebd623285130a1
Peng <hzp1024@qq.com>
2024-10-18 20:12:40 +0800

    Update toc-desktop.js

--- _javascript/modules/components/toc/toc-desktop.js
+++ _javascript/modules/components/toc/toc-desktop.js
@@ -3,11 +3,10 @@ export class TocDesktop {
   static options = {
     tocSelector: '#toc',
     contentSelector: '.content',
-    /* ignoreSelector: '[data-toc-skip]', */ 
     headingSelector: 'h1, h2, h3, h4',
     orderedList: false,
     scrollSmooth: false,
-    /* headingsOffset: 16 * 2, // 2rem */
+    headingsOffset: 16 * 2, // 2rem
     collapseDepth: 6
   };
 
