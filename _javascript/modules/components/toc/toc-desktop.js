6ea5143b25dec91162bfd8ec0ba29c90c304a8f2
Peng <hzp1024@qq.com>
2024-10-18 20:14:47 +0800

    Update toc-desktop.js

--- _javascript/modules/components/toc/toc-desktop.js
+++ _javascript/modules/components/toc/toc-desktop.js
@@ -1,13 +1,13 @@
 export class TocDesktop {
   /* Tocbot options Ref: https://github.com/tscanlin/tocbot#usage */
   static options = {
-    tocSelector: '#toc',
-    contentSelector: '.content',
-    headingSelector: 'h1, h2, h3, h4',
-    orderedList: false,
-    scrollSmooth: false,
-    headingsOffset: 16 * 2, // 2rem
-    collapseDepth: 6
+        tocSelector: '#toc',
+        contentSelector: '.content',
+        ignoreSelector: '[data-toc-skip]',
+        headingSelector: 'h2, h3, h4',
+        orderedList: false,
+        scrollSmooth: false,
+        collapseDepth: 6,
   };
 
   static refresh() {
